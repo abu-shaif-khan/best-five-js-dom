ebbad8218a847ffd75131296bb8c4cd48877cda0
abu-shaif-khan <askhan@rhrk.uni-kl.de>
2023-04-18 14:09:37 +0200

    button id added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// player button function
+
+function getPlayerButton(elementId){
+    let selectBtn = document.getElementById(elementId);
+
+}
+
+document.getElementById('player1').addEventListener('click', function(){
+    
+})
